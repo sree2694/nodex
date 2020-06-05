b132308873f498c3a9534445b1dd5f59d75a96ff
sree2694 <58017164+sree2694@users.noreply.github.com>
2020-06-05 07:58:21 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var a = 7;
+var b=  10;
+
+var c = a+b;
+
+console.log("The output is : " + c);
---
+++ calc/app.js
@@ -0,0 +1,10 @@
+var calc = require('./calc.js')
+
+result = calc.add(4,5);
+
+console.log("The Ouput is : " + result)
+
+result1 = calc.sub(4,5);
+
+console.log("The Ouput is : " + result1)
+
